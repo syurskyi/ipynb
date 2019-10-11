3c3cf9f3207314639f1129cf77e01d13685154e9
Grey <sergejyurskyj@yahoo.com>
2019-10-11 08:28:26 -0700

    commit set_template

---
+++ set_template.py
@@ -0,0 +1,235 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Create set
+# s
+# print(_)
+
+# Преобразуем строку in set
+# print _ string
+
+# Преобразуем список in set
+# print_ 1, 2, 3, 4, 5
+
+# Преобразуем кортеж in set
+# print __1, 2, 3, 4, 5
+
+# Остаются только уникальные элементы
+# print __1, 2, 3, 1, 2, 3
+
+# len()
+# print(_(___1 2 3
+
+# union()
+s = set([1, 2, 3])
+# print(_._(_ __4, 5, 6___, _ _ __ __4, 5, 6___
+
+# update()
+s = set([1, 2, 3])
+# _._(_ __4 5 6___
+print(_)
+
+# difference()
+
+# print(__ __1 2 3__ _ _ __1 2 4___
+
+# s = set([1, 2, 3])
+# print(_._(_ __1 2 4____
+
+# intersection()
+s = set([1, 2, 3])
+# print(_ __1 2 3__ _ _ __1 2 4___
+
+s = set([1, 2, 3])
+# print(_._(_ __1 2 4_____
+
+# symmetric_difference
+s = set([1, 2, 3])
+# print(_ _ _ __1 2 4__, _._(_ __1 2 4____
+
+# in
+s = set([1, 2, 3, 4, 5])
+# print(1 , 12)
+
+# x in s
+s = set([1, 2, 3, 4, 5])
+# print(1 , 12)
+
+# x not in s
+
+# ==
+# print(set([1, 2, 3]) == set([1, 2, 3]))
+# True
+# print(set([1, 2, 3]) == set([3, 2, 1]))
+# True
+# print(set([1, 2, 3]) == set([1, 2, 3, 4]))
+# False
+
+# issubset or <=
+s = set([1, 2, 3])
+# print(s __ _([1, 2]), s __ _([1, 2, 3, 4]))
+
+# print(s._(_([1, 2])), s._(_([1, 2, 3, 4])))
+
+# issuperset or =>
+s = set([1, 2, 3])
+# print(s _ _([1, 2]), s _ _([1, 2, 3, 4]))
+# print(s._(_([1, 2])), s._(_([1, 2, 3, 4])))
+# (True, False)
+
+# isdisjoint()
+s = set([1, 2, 3])
+# print(s._(_([4, 5, 6])))
+# print(s._(_([1, 3, 5])))
+
+# Creating Sets
+# s = a 100 (1 2)
+# _(s)
+
+# create an empty set
+# s
+_(_)
+
+# Adding Elements
+s = {30, 20, 10}
+# 15
+
+# Removing Elements
+s = {1, 2, 3}
+# 1
+# 10 error
+# # # # If we don't want the exception we can do it this way:
+# 10
+
+
+# pop()
+s = set('python')
+# print(_)
+#
+# print(_)
+
+# clear()
+s = {1, 2, 3}
+#
+# print(_)
+
+# Set Operations
+# Intersections
+
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+
+# Set Operations
+# Unions
+s1 = {1, 2, 3}
+s2 = {3, 4, 5}
+s1.intersection(s2)
+s1 & s2
+
+# Set Operations
+# Disjointedness
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = {30, 40, 50}
+
+# # # # Of course we could use the cardinality of the intersection instead:
+
+
+# # # # we can also use the associated truth value:
+
+
+# Set Operations
+# Differences
+
+s1 = {1, 2, 3, 4, 5}
+s2 = {4, 5}
+
+# Set Operations
+# Symmetric Difference
+
+s1 = {1, 2, 3, 4, 5}
+s2 = {4, 5, 6, 7, 8}
+
+# Set Operations
+# Subsets and Supersets
+
+s1 = {1, 2, 3}
+s2 = {1, 2, 3}
+s3 = {1, 2, 3, 4}
+s4 = {10, 20, 30}
+
+# # # # For strict containment there is no set method - we have to use the operator, or a combination of methods/operators:
+
+
+# # # # An analogous situation with supersets:
+
+
+# Union Updates
+
+s1 = {1, 2, 3}
+s2 = {4, 5, 6}
+
+
+# Intersection Updates
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+
+# Difference Updates
+s1 = {1, 2, 3, 4}
+s2 = {2, 3}
+
+# Symmetric Difference Update
+
+s1 = {1, 2, 3, 4, 5}
+s2 = {4, 5, 6, 7}
+
+
+# Пустое неизменяемое множество
+
+# Создание множеств из итерабельых объектов
+
+my_frozenset = None
+
+my_set = None
+
+# set_comprehensions
+
+number_set = None
+
+char_set = None
+
+# Количество элементов множества
+
+my_set = {4, 5, 1, 2}
+
+# Проверка вхождения элемента
+
+my_set = {4, 5, 1, 2}
+
+
+# Пересекаются ли множества
+
+
+# Проверка включения одного множества в другое
+
+
+# Проверка строгого включения
+
+# Проверка включения одного множества в другое
+
+# Проверка строгого включения
+
+# Объединение множеств
+
+# Пересечение множеств
+
+# Разница множеств
+
+# Симметрическая разница
+
+# Копирование множества
+
+my_set = None
+copy = None
+
+
